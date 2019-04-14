5a02692db8d6ce66e5d97afdde96e35eadb5cc4a
alex.bruns <alex.h.bruns@gmail.com>
2019-04-14 02:35:01 -0500

    AB :: errors, wip, and root page

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask
+from flask import Flask, render_template
+import jinja2
 import sqlite3
 
 
@@ -7,7 +8,22 @@ app = Flask(__name__)
 
 @app.route("/")
 def root():
-    return "server is live"
+    return render_template("root.html")
+
+
+@app.route("/error_4xx")
+def error_4xx():
+    return render_template("noncontent/error_4XX.html")
+
+
+@app.route("/error_5xx")
+def error_5xx():
+    return render_template("noncontent/error_5XX.html")
+
+
+@app.route("/wip")
+def wip():
+    return render_template("noncontent/wip.html")
 
 
 if __name__ == "__main__":
