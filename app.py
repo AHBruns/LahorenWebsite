5c08d717f07f1b5af6602c6012a4ff5fe52e7c93
alex.bruns <alex.h.bruns@gmail.com>
2019-04-15 12:41:01 -0500

    AB :: login, uploading, downloading, and other stuff

--- app.py
+++ app.py
@@ -1,14 +1,105 @@
-from flask import Flask, render_template
-import jinja2
+from flask import Flask, render_template, request, flash, redirect, session, send_from_directory, url_for
+from werkzeug.utils import secure_filename
+import os
 import sqlite3
+import random
+import hashlib
+
+
+UPLOAD_FOLDER = '/Users/alex.bruns/LahorenWebsite/uploads'
+ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
 
 
 app = Flask(__name__)
+app.secret_key = os.urandom(24)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+
+@app.route('/login', methods=["GET", "POST"])
+def do_admin_login():
+    print(request.url)
+    if request.method == "GET":
+        return render_template("login.html")
+    if request.form['password'] == 'password' and request.form['username'] == 'admin':
+        session['logged_in'] = True
+    else:
+        flash('wrong password!')
+    if "url" in session:
+        return redirect(session["url"])
+    return redirect("/")
 
 
 @app.route("/")
 def root():
+    session['url'] = url_for("root")
     return render_template("root.html")
+
+
+@app.route("/downloads")
+def downloads():
+    session['url'] = url_for("downloads")
+    conn = sqlite3.connect("db.sqlite3")
+    c = conn.cursor()
+    c.execute("""SELECT * FROM Uploads""")
+    fns = c.fetchall()
+    conn.close()
+    dls = []
+    for fn in fns:
+        dls.append({
+            "link": "uploads/"+fn[0],
+            "title": fn[0][67:]
+        })
+    return render_template(
+        "downloads.html",
+        dls=dls
+    )
+
+
+def allowed_file(filename):
+    return '.' in filename \
+           and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+@app.route("/upload", methods=["GET", "POST"])
+def upload():
+    session['url'] = url_for("upload")
+    if not session.get('logged_in'):
+        return redirect("/login")
+    if request.method == "GET":
+        return render_template("upload.html")
+    else:
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit an empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            h = hashlib.sha256()
+            h.update(str(random.getrandbits(100)).encode("utf-8"))
+            filename = h.hexdigest() + "___" + filename
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            conn = sqlite3.connect("db.sqlite3")
+            c = conn.cursor()
+            c.execute("""INSERT INTO Uploads VALUES (?)""", (filename,))
+            conn.commit()
+            conn.close()
+            return "done"
+
+
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory('uploads', filename)
+
+
+@app.route("/static/assets/<filename>")
+def assets(filename):
+    return send_from_directory('static/assets', filename)
 
 
 @app.route("/error_4xx")
@@ -27,4 +118,5 @@ def wip():
 
 
 if __name__ == "__main__":
+    app.secret_key = os.urandom(24)
     app.run(port=5000)
--- static/js/root.js
+++ static/js/root.js
@@ -10,14 +10,88 @@ let names = [
     "Coffee Consumer"
 ];
 let names_idx = 0;
+let angle = 0;
+let zoom = 1;
+let tick = 1;
 
 function type_names() {
-    console.log("upadting name");
+    console.log("updating name");
     let el = document.getElementById("Alex_Bruns_Label");
     el.innerText = "Alex Bruns: " + names[names_idx % names.length];
     names_idx++;
 }
 
+
+var boxMullerRandom = (function () {
+    var phase = 0,
+        RAND_MAX,
+        array,
+        random,
+        x1, x2, w, z;
+
+    if (crypto && typeof crypto.getRandomValues === 'function') {
+        RAND_MAX = Math.pow(2, 32) - 1;
+        array = new Uint32Array(1);
+        random = function () {
+            crypto.getRandomValues(array);
+
+            return array[0] / RAND_MAX;
+        };
+    } else {
+        random = Math.random;
+    }
+
+    return function () {
+        if (!phase) {
+            do {
+                x1 = 2.0 * random() - 1.0;
+                x2 = 2.0 * random() - 1.0;
+                w = x1 * x1 + x2 * x2;
+            } while (w >= 1.0);
+
+            w = Math.sqrt((-2.0 * Math.log(w)) / w);
+            z = x1 * w;
+        } else {
+            z = x2 * w;
+        }
+
+        phase ^= 1;
+
+        return z;
+    }
+}());
+
+let v = 0;
+let t = 0;
+
+function randomWalk(steps, randFunc) {
+    steps = steps >>> 0 || 100;
+    if (typeof randFunc !== 'function') {
+        randFunc = boxMullerRandom;
+    }
+
+    var points = [],
+        value = 0,
+        t;
+
+
+
+    return points;
+}
+
+function random_transform() {
+    let e = document.getElementById("page");
+    v += (boxMullerRandom() / 10000.0);
+    // angle = v*360;
+    zoom = zoom / (1 + (v/100) + (tick/10000000));
+    console.log(v);
+    tick++;
+    e.style.transform = "rotate(" + angle.toString() + "deg) " + "scale(" + zoom.toString() +")";
+
+
+}
+
 window.onload = function () {
-    setInterval("type_names()", 1000)
+    // setTimeout("setInterval(\"random_transform()\", 1)", 5000);
+    setInterval("type_names()", 1000);
 };
